19f37320fa1e77c125a0e6bb8aa6a0d383bf68a0
almeidagit <msnpaulo@msn.com>
2021-04-08 01:26:03 -0300

    update 1

---
+++ src/main/java/com/contactura/contactura/ContacturaApplication.java
@@ -0,0 +1,13 @@
+package com.contactura.contactura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContacturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContacturaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/contactura/contactura/ContacturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contactura.contactura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContacturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
